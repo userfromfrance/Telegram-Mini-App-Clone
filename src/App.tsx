bc95482806d64d181bbe6f5aa2da9e4b1ef4ed72
userfromfrance <matheojorigne@hotmail.fr>
2025-01-19 21:02:24 +0000

    modif

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './index.css';
 import Arrow from './icons/Arrow';
-import { bear, coin, highVoltage, notcoin, rocket, trophy } from './images';
+import { bear, coin, highVoltage, krokodil, rocket, trophy } from './images';
 
 const App = () => {
   const [points, setPoints] = useState(29857775);
@@ -101,7 +101,7 @@ const App = () => {
 
         <div className="flex-grow flex items-center justify-center">
           <div className="relative mt-4" onClick={handleClick}>
-            <img src={notcoin} width={256} height={256} alt="notcoin" />
+            <img src={krokodil} width={256} height={256} alt="krokodil" />
             {clicks.map((click) => (
               <div
                 key={click.id}
--- src/images/index.ts
+++ src/images/index.ts
@@ -1,7 +1,7 @@
 import bear from "./bear.png";
 import coin from "./coin.png";
 import highVoltage from "./high-voltage.png";
-import notcoin from "./notcoin.png";
+import krokodil from "./krokodil.png";
 import rocket from "./rocket.png";
 import trophy from "./trophy.png";
 
@@ -9,7 +9,7 @@ export {
     bear,
     coin,
     highVoltage,
-    notcoin,
+    krokodil,
     rocket,
     trophy
 }
